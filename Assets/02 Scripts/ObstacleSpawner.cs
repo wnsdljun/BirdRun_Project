e3a565478d0bc55cffa8a662341ab05dadb70146
geroer <110544587+geroer@users.noreply.github.com>
2025-02-24 16:41:44 +0900

    [Feat] 장애물 랜덤 생성 및 삭제 기능

--- Assets/02 Scripts/ObstacleSpawner.cs
+++ Assets/02 Scripts/ObstacleSpawner.cs
@@ -4,45 +4,34 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    public GameObject[] obstacles;
+    public GameObject[] obstaclesList;  //장애물리스트
+
     public GameObject ground;
     public Transform lastObstacle;
+    public GameObject curObjstacle;
 
     private int groundCnt = 0;
     private bool isObstacle = false;
 
-    void SpawnGround()
+    //Camera cam;   //테스트용 카메라
+
+    private void Awake()
     {
-        Vector3 newPosition = lastObstacle.position + new Vector3(3f, 0, 0);
-        GameObject newGround = Instantiate(ground, newPosition, Quaternion.identity);
-        lastObstacle = newGround.transform;
-
-        groundCnt++;
-
-        isObstacle = false;
+        Obstacle[] curObjstacle = GameObject.FindObjectsOfType<Obstacle>();
     }
-    void SpawnObstacle()
-    {
-        if (groundCnt < 2) return;
-
-        Vector3 newPosition = lastObstacle.position + new Vector3(3f, 0, 0);
-
-        int randomIdx = Random.Range(0, obstacles.Length);
-        GameObject newObstacle = Instantiate(obstacles[randomIdx], newPosition, Quaternion.identity);
-        lastObstacle = newObstacle.transform;
-
-        groundCnt = 0;
-
-        isObstacle = true;
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 10; i++)
+        //cam = Camera.main;    //테스트용 카메라
+    }
+
+    void Update()
+    {
+        //cam.transform.position += Vector3.right * 4f * Time.deltaTime;    //테스트용 카메라
+
+        //생성된 Obstacle이 20개 이하일 때만 추가로 생성
+        if (curObjstacle.transform.childCount < 20)
         {
-            //if (!isObstacle && Random.value > 0.7f) // 30% 확률로 장애물 생성
-            if (!isObstacle)
+            if (!isObstacle && Random.value > 0.5f) // 50% 확률로 장애물 생성
             {
                 SpawnObstacle();
             }
@@ -53,9 +42,33 @@ public class ObstacleSpawner : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    //바닥 생성
+    void SpawnGround()
     {
-        
+        Vector3 newPosition = lastObstacle.position + new Vector3(3f, 0, 0);
+        GameObject newGround = Instantiate(ground, newPosition, Quaternion.identity);
+        newGround.transform.SetParent(curObjstacle.transform);
+        lastObstacle = newGround.transform;
+
+        groundCnt++;
+
+        isObstacle = false;
+    }
+
+    //장애물 생성
+    void SpawnObstacle()
+    {
+        if (groundCnt < 2) return;
+
+        Vector3 newPosition = lastObstacle.position + new Vector3(3f, 0, 0);
+
+        int randomIdx = Random.Range(0, obstaclesList.Length);
+        GameObject newObstacle = Instantiate(obstaclesList[randomIdx], newPosition, Quaternion.identity);
+        newObstacle.transform.SetParent(curObjstacle.transform);
+        lastObstacle = newObstacle.transform;
+
+        groundCnt = 0;
+
+        isObstacle = true;
     }
 }
