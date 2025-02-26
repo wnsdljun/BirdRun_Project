302c0c43d06112a919f3d5888d455e9d2bad348d
geroer <110544587+geroer@users.noreply.github.com>
2025-02-26 10:39:25 +0900

    [Fix] 자석 사용 메서드 호출 추가

--- Assets/02 Scripts/Item/Magnet.cs
+++ Assets/02 Scripts/Item/Magnet.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Magnet : MonoBehaviour, IItemEffect
 {
+    public float duration = 5f;
+
     public void ApplyEffect(Player player)
     {
-
+        player.UseMagnet(duration);
     }
 }
