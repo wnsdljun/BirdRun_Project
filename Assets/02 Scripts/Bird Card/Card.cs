3791efc571b71c8fd8590686599884cab553c47e
wnsdljun <118675157+wnsdljun@users.noreply.github.com>
2025-02-26 23:47:32 +0900

    [Fix] 카드 뒤집는 로직 수정
    버그픽스 + 흔들리는 효과

--- Assets/02 Scripts/Bird Card/Card.cs
+++ Assets/02 Scripts/Bird Card/Card.cs
@@ -5,26 +5,34 @@ using UnityEngine.UI;
 public class Card : MonoBehaviour
 {
     [SerializeField] GameObject character;
+    [SerializeField] bool selectable;
     [SerializeField] float flipDuration = 0.5f;
-    Button button;
-
+    Vector2 originPos;
     bool isFlip = false;
-    bool canClick = true;
+    private void Start()
+    {
+        originPos = transform.position;
+    }
     private void Update()
     {
-        if (canClick && Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0))
         {
-            canClick = false;
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
 
             if (hit.collider != null)
             {
                 Card card = hit.collider.GetComponent<Card>();
-                if (card != null)
+                if (card == this)
                 {
-                    card.ClickCard();
-                    return;
+                    if (card.selectable)
+                    {
+                        card.ClickCard();
+                    }
+                    else
+                    {
+                        card.StartCoroutine(UnavailableCardClick());
+                    }
                 }
             }
         }
@@ -54,7 +62,24 @@ public class Card : MonoBehaviour
             rotY = Mathf.Lerp(0f, 180f, elapsed / flipDuration);
             transform.rotation = Quaternion.Euler(0, rotY, 0);
             elapsed += Time.deltaTime;
-            canClick = !(elapsed < flipDuration);
+            yield return null;
+        }
+        yield return null;
+    }
+
+    IEnumerator UnavailableCardClick()
+    {
+        float time = 1f;
+        float elapsed = 0f;
+
+        while (elapsed < time)
+        {
+            elapsed += Time.deltaTime;
+            transform.position = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) + originPos;
+            if (!(elapsed < time))
+            {
+                transform.position = originPos;
+            }
             yield return null;
         }
     }
