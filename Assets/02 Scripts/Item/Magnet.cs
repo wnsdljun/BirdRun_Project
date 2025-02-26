f98d7600dd4ce85dafc47c5f857412de86950a3d
geroer <110544587+geroer@users.noreply.github.com>
2025-02-26 09:58:26 +0900

    [Feat] 자석 아이템 추가

---
+++ Assets/02 Scripts/Item/Magnet.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour, IItemEffect
+{
+    public void ApplyEffect(Player player)
+    {
+
+    }
+}
